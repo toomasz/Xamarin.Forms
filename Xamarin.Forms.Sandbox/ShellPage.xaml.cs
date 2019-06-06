8bd02d2d972fc85e6ebe202e3e0b4868b6073247
ShaneN <shane94@hotmail.com>
2019-06-06 15:40:24 -0600

    test

--- Xamarin.Forms.Sandbox/ShellPage.xaml.cs
+++ Xamarin.Forms.Sandbox/ShellPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Forms.Sandbox
@@ -12,6 +14,122 @@ namespace Xamarin.Forms.Sandbox
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ShellPage : Shell
 	{
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			var page = new ContentPage()
+			{
+				Content =
+					new ScrollView()
+					{
+						Content = new StackLayout()
+						{
+							Children =
+							{
+								new Label()
+								{
+									Text = "Top Label"
+								},
+
+								new Label()
+								{
+									Text = "test"
+								},
+
+								new Label()
+								{
+									Text = "test"
+								},
+
+								new Label()
+								{
+									Text = "Do you see the Top Label?"
+								},
+								new Button()
+								{
+									Text = "Pop The Page",
+									Command = new Command(async () =>
+									{
+										await Navigation.PopAsync();
+									})
+								},
+							}
+						}
+					}
+			};
+
+			Shell.SetNavBarIsVisible(page, isChecked.IsChecked);
+			page.On<PlatformConfiguration.iOS>().SetUseSafeArea(false);
+
+			if (paddingToUse().HasValue)
+			{
+				page.Padding = new Thickness(0, paddingToUse().Value, 0, 0);
+			}
+
+			await Navigation.PushAsync(page);
+		}
+
+
+
+		async void Handle2_Clicked(object sender, System.EventArgs e)
+		{
+			var page = new ContentPage()
+			{
+				Content = new StackLayout()
+				{
+					Children =
+					{
+						new Label()
+						{
+							Text = "top"
+						},
+						new Label()
+						{
+							Text = "test1"
+						},
+
+						new Label()
+						{
+							Text = "test1"
+						},
+
+						new Label()
+						{
+							Text = "test1"
+						},
+
+						new Label()
+						{
+							Text = "test1"
+						},
+
+						new Label()
+						{
+							Text = "test1"
+						},
+					}
+				}
+			};
+
+			if(paddingToUse().HasValue)
+			{
+				page.Padding = new Thickness(0, paddingToUse().Value, 0, 0);
+			}
+
+			Shell.SetBackgroundColor(page, Color.Purple);
+			Shell.SetNavBarIsVisible(page, isChecked.IsChecked);
+			page.On<PlatformConfiguration.iOS>().SetUseSafeArea(false);
+			await Navigation.PushAsync(page);
+		}
+
+		int? paddingToUse()
+		{
+			int thing;
+
+			if (int.TryParse(PaddingToUse.Text, out thing))
+				return thing;
+
+			return null;
+		}
 		public ShellPage()
 		{
 			InitializeComponent();
